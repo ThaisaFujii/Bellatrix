613e72648e8e6f87389d9d64b2416c6aaaf2fd0b
Reses <reseflix@hotmail.com>
2021-08-10 00:10:29 -0300

    v0.01

---
+++ Javascript/Scenes/SceMenu.js
@@ -0,0 +1,10 @@
+const Phaser = require("../../phaser2");
+
+export default class SceMenu extends Phaser.Scene {
+    constructor() {
+        super({key:"SceMenu"});
+    }
+    preloader() {}
+    create() {}
+    update() {}
+}
